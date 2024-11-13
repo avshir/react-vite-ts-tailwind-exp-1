ff6868885b0996543dfe2b6808a91fdebb5ab9aa
Anna Shirinskaya <av.shirinskaya@gmail.com>
2024-11-13 13:44:02 +0100

    feat: add articles data

---
+++ src/data/articles.ts
@@ -0,0 +1,19 @@
+interface IArticles {
+  id: number,
+  title: string,
+  text: string,
+}
+
+export const articles: IArticles[] = [
+  {
+    id: 1,
+    title: 'Article 1',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptatem eveniet odio veritatis totam cum in dolore tempore quaerat aliquam blanditiis, labore name.',
+  },
+  { id: 2, title: 'Article 2', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.' },
+  {
+    id: 3,
+    title: 'Article 3',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptatem eveniet odio veritatis totam cum in dolore tempore quaerat aliquam blanditiis, labore nam. Eos sitiure magnam quibusdam omnis obcaecati, repellat quisquam, quasi, consequuntur quas earumreprehenderit veniam alias totam minus et nobis. Reprehenderit consequatur id, optioaccusantium earum vero aliquid dicta tempora, sint hic et vitae illo assumenda ipsaincidunt ab veniam quisquam corporis voluptatem deserunt. Aliquid ducimus molestiaeadipisci consequuntur, harum sint repellat optio voluptates provident sit deseruntitaque labore autem accusamus officia nemo ab eaque modi in eos suscipit exercitationemvoluptatum nam quod? Mollitia officiis maxime asperiores beatae! Excepturi, cum nemoprovident adipisci voluptates perspiciatis distinctio, recusandae voluptatenecessitatibus aut repellendus nulla nihil soluta. In, saepe? At, libero nesciunt.Impedit, vero reiciendis!',
+  },
+];

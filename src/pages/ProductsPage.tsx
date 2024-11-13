b8e744fe8fc3545e52b1aa542d54bf1839397407
Anna Shirinskaya <av.shirinskaya@gmail.com>
2024-11-13 13:57:47 +0100

    feat: add search section in ProductsPage

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, ChangeEvent } from 'react';
+
 import CreateProduct from '../components/CreateProduct';
 import ErrorMessage from '../components/ErrorMessage';
 import { Loader } from '../components/Loader';
@@ -11,6 +12,12 @@ import { IProduct } from '../models';
 export default function ProductsPage() {
   const { products, isLoading, error, addProduct } = useProducts();
   const [isModal, setIsModal] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const newSearchValue = event.target.value.trimStart();
+    setSearchValue(newSearchValue);
+  };
 
   const createProductHandler = (product: IProduct) => {
     setIsModal(false);
@@ -20,7 +27,7 @@ export default function ProductsPage() {
   return (
     <main className="container mx-auto p-8">
       <h1>Products Page</h1>
-      <h2 className="text-base font-bold">Used: </h2>
+      <h4 className="text-base font-bold">Used: </h4>
       <ul className="list-disc pl-4 text-base">
         <li>
           <a href="https://fakestoreapi.com" target="_blank">
@@ -37,10 +44,26 @@ export default function ProductsPage() {
       </button>
       {isLoading && <Loader />}
       {error && <ErrorMessage textError={error} />}
+      <div className="search-product-section">
+        <label htmlFor="search-product" className="text-base font-bold text-yellow-700">
+          Search by title:{' '}
+        </label>
+        <input
+          id="search-product"
+          type="text"
+          placeholder="Search by title product"
+          className="mb-2 mr-2 rounded border px-4 py-2"
+          value={searchValue}
+          onChange={(e) => onSearchChange(e)}
+        />
+      </div>
+
       <div className="mx-auto grid max-w-[1024px] grid-cols-1 gap-2 py-4 sm:grid-cols-2 md:grid-cols-3 lg:gap-4">
-        {products.map((product) => (
-          <Product product={product} key={product.id} />
-        ))}
+        {products
+          .filter((product) => product.title.toLowerCase().includes(searchValue.toLowerCase()))
+          .map((product) => (
+            <Product product={product} key={product.id} />
+          ))}
       </div>
       {isModal && (
         <Modal title="Create new product" onClose={() => setIsModal(false)}>

b26e18151dc25f9200afc10c9729b58936119959
Anna Shirinskaya <av.shirinskaya@gmail.com>
2024-11-14 15:33:29 +0100

    feat: update ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function ErrorPage() {
   return (
-    <div id="error-page" className="flex h-[80vh] flex-col items-center justify-center">
+    <div
+      id="error-page"
+      className="container mx-auto flex h-[80vh] flex-col items-center justify-center px-8"
+    >
       <h1>Ooops!</h1>
       <p className="text-2xl font-bold text-purple-700">Sorry, this page is 404.</p>
-      <Link to="/" className="text-2xl bg-purple-700 text-white px-6 py-4 rounded-2xl">Home Page</Link>
+      <Link
+        to="/"
+        className="w-full text-center rounded-2xl bg-purple-700 px-6 py-4 text-2xl text-white sm:w-40"
+      >
+        Home Page
+      </Link>
     </div>
   );
 }

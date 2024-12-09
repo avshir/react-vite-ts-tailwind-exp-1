710d840567a5577966ff6fe180353b369b1a3c2a
avshir <66124089+avshir@users.noreply.github.com>
2024-12-09 13:49:14 +0100

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,3 +8,18 @@ export function cn(...inputs: ClassValue[]) {
 export function classNames(...classes: (string | undefined | false)[]): string {
   return classes.filter(Boolean).join(' ');
 }
+
+export function formatDate(dateString: string) {
+  return new Date(`${dateString}T00:00:00Z`).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    timeZone: "UTC",
+  });
+}
+
+export const isMobile = () => {
+  if (typeof window === "undefined") return false;
+  const width = window.innerWidth;
+  return width <= 1024;
+};
